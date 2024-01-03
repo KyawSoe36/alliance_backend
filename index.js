4fe6a41d8f8b78dbd0ce2d577261fc48fbccd009
Kyaw Soe <kyawsoe.cumdy2018@gmail.com>
2024-01-03 11:53:50 +0630

    add configuration port by env file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Sample Api!')
-})
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Sample Api!");
+});
 
 app.listen(port, () => {
-  console.log(`App is running on ${port}`)
-})
+  console.log(`App is running on ${port}`);
+});
